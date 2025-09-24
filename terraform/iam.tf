bf8e8505f5086578be8d4f9174535a0315cdad4b
Cornel Bacanu <cornelbacanu@gmail.com>
2025-09-24 21:47:25 +0100

    SEcondFix

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -9,59 +9,7 @@ resource "aws_iam_role_policy" "lambda_policy" {
   policy = file("${path.module}/files/iam/lambda_policy.json")
 }
 
-resource "aws_iam_role" "github_actions_role" {
-  name = "GitHubActionsRole"
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Effect = "Allow"
-        Principal = {
-          Federated = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:oidc-provider/token.actions.githubusercontent.com"
-        }
-        Action = "sts:AssumeRoleWithWebIdentity"
-        Condition = {
-          StringEquals = {
-            "token.actions.githubusercontent.com:aud" = "sts.amazonaws.com"
-          }
-        }
-      }
-    ]
-  })
-}
 
-resource "aws_iam_role_policy" "github_actions_policy" {
-  name = "GitHubActionsPolicy"
-  role = aws_iam_role.github_actions_role.id
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Effect = "Allow"
-        Action = [
-          "lambda:GetFunction",
-          "lambda:UpdateFunctionCode",
-          "lambda:UpdateFunctionConfiguration",
-          "lambda:CreateFunction",
-          "lambda:DeleteFunction",
-          "lambda:InvokeFunction",
-          "lambda:ListFunctions",
-          "lambda:GetLayerVersion",
-          "lambda:PublishLayerVersion",
-          "lambda:DeleteLayerVersion",
-          "s3:GetObject",
-          "s3:PutObject",
-          "s3:DeleteObject",
-          "s3:ListBucket",
-          "iam:PassRole"
-        ]
-        Resource = "*"
-      }
-    ]
-  })
-}
-
-data "aws_caller_identity" "current" {}
 
 
 
